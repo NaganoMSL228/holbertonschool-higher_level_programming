80e7238607c11d7e28030cd4c80af2e824cc4eb5
NaganoMSL228 <9794@holbertonstudents.com>
2025-01-13 23:24:23 +0100

    change the script in task

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = "object-oriented programming with Python"
-str = str[:26] + str[27:32] + str[-6:]
-print(str)
+print(str[39:67] + str[107:112] + str[:6])
