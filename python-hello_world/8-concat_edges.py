e5f3c568d3e117d298d2ce8a5f1aa7577812367f
NaganoMSL228 <9794@holbertonstudents.com>
2025-01-13 23:17:06 +0100

    just test

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
 str = "object-oriented programming with Python"
 str = str[:26] + str[27:32] + str[-6:]
 print(str)
