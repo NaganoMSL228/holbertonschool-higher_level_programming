4420f532132db31134ca7c553d1102994ee1a582
NaganoMSL228 <9794@holbertonstudents.com>
2025-01-14 16:45:44 +0100

    change the script task advenced

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 for i in range(26):
-    char = chr(122 - i) if i % 2 == 0 else chr(90 - i)  # 122 is 'z', 90 is 'Y'
-    print(char, end='')
+    print("{:c}".format(122 - i - (i % 2 * 32)), end="")
