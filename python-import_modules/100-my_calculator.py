221df44580d207e7dc9284d1f5bc66f535fb6d1e
NaganoMSL228 <9794@holbertonstudents.com>
2025-01-16 12:40:56 +0100

    chande import

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
 from sys import argv, exit
+from calculator_1 import add, sub, mul, div
 
 
-def main():
+if __name__ == "__main__":
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    a = int([1])
-    op = [2]
-    b = int([3])
 
-    if op == '+':
+    a = int(argv[1])
+    operator = argv[2]
+    b = int(argv[3])
+
+    if operator == '+':
         result = add(a, b)
         print(f"{a} + {b} = {result}")
-    elif op == '-':
+    elif operator == '-':
         result = sub(a, b)
         print(f"{a} - {b} = {result}")
-    elif op == '*':
+    elif operator == '*':
         result = mul(a, b)
         print(f"{a} * {b} = {result}")
-    elif op == '/':
+    elif operator == '/':
+        if b == 0:
+            print("Error: Division by zero")
+            exit(1)
         result = div(a, b)
         print(f"{a} / {b} = {result}")
     else:
-        print('Unknown operator. Available operators: +, -, * and /')
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-
-if __name__ == "__main__":
-    main()
