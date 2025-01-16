08e731e4cce4181733ce7736c1a3a5f4bc3b8f23
NaganoMSL228 <9794@holbertonstudents.com>
2025-01-16 10:44:02 +0100

    call the new fonction

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 import sys
-
+def greet(main)
 if __name__ == "__main__":
     if len(sys.argv) != 4:
+        greet(main=0)
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
