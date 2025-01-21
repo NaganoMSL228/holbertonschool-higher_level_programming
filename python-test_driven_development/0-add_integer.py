5b0709eed784c33fe5bf3f1efc58a53285673f35
NaganoMSL228 <9794@holbertonstudents.com>
2025-01-21 02:44:06 +0100

    create full files .py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-"""
-This module contains a function that adds two integers or floats
-"""
-
 
 def add_integer(a, b=98):
-    """
-    This function adds two integers or floats
-    """
+    """Add two integers or floats together"""
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
