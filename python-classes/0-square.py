b650b9df03c3e4be0fb59b11f8f87c1915527845
NaganoMSL228 <9794@holbertonstudents.com>
2025-01-27 00:19:49 +0100

    teste for the task one

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,34 @@
 #!/usr/bin/python3
+"""
+This module defines a Car class that represents a car object,
+with attributes for make and model.
+"""
 
 
 class Car:
+    """
+    A class that represents a car.
+
+    Attributes:
+        make (str): The make of the car.
+        model (str): The model of the car.
+    """
+
     def __init__(self, make, model):
-        self.make = make
-        self.model = model
+        """
+        Initializes a new Car instance with the specified make and model.
+
+        Args:
+            make (str): The make of the car.
+            model (str): The model of the car.
+        """
+        self.make = make  # Public attribute
+        self.model = model  # Public attribute
+
+# Example of creating an instance of the Car class
+
+
+if __name__ == "__main__":
+    my_car = Car("Toyota", "Corolla")
+    print(f"My car is a {my_car.make} {my_car.model}.")
+    print(my_car.__dict__)  # Displaying the attributes of the instance
