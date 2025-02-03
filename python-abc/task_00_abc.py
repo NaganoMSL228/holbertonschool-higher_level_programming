a574a38c2253b2d5012311c2a69d0e196c74386f
NaganoMSL228 <9794@holbertonstudents.com>
2025-02-03 16:40:23 +0100

    test for the task 00 in new projet 2 oh week

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from task_00_abc import Animal, Dog, Cat
+
+chien = Dog()
+chat = Cat()
+
+print(chien.sound())
+print(chat.sound())
+
+animal = Animal()
+print(animal.sound())
