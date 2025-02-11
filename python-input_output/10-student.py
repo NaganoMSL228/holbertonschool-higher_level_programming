6e4a2caa08f78b425f8997edaf2c0bc6c04cc5b3
NaganoMSL228 <9794@holbertonstudents.com>
2025-02-11 23:48:12 +0100

    new script for task 10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-""" Student module
-"""
+"""Defines a Student class."""
 
 
 class Student:
-    """ Student class
-    """
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
-        """ initializes student instance
-        """
+        """Initialize a new Student."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """ returns dictionary representation of student instance
-        """
-        return self.__dict__
+    def to_json(self, attrs=None):
+        """Get dictionary representation of
+        Student with attribute filtering."""
+        if attrs is None:
+            return self.__dict__
+        return {k: v for k, v in self.__dict__.items() if k in attrs}
+
+    def reload_from_json(self, json):
+        """Method that replaces all attributes of the Student instance."""
+        for key, value in json.items():
+            setattr(self, key, value)
