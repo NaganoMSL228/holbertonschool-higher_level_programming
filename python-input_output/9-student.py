f8ea8efbfc17c6436ccd7400e0295f67d9e40be8
NaganoMSL228 <9794@holbertonstudents.com>
2025-02-11 23:43:08 +0100

    test no with copilot

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-'''Module for Student class'''
-
-import json
+'''module for student class'''
 
 
 class Student:
-    '''Class Student'''
+    '''student class'''
     def __init__(self, first_name, last_name, age):
-        '''Constructor for Student'''
+        '''initializes student instance'''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        '''Returns dictionary representation of a Student instance'''
+        '''returns dictionary representation of student instance'''
         return self.__dict__
