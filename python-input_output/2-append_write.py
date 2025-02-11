2e83b1c2cf80c9c793a567ce568c032564825e9a
NaganoMSL228 <9794@holbertonstudents.com>
2025-02-11 22:45:41 +0100

    corect pycodestyle

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,7 +2,9 @@
 
 '''Module the append_write function'''
 
+
 def append_write(filename="", text=""):
-    '''Appends a string at the end of a text file (UTF8) and returns the number of characters added'''
+    '''Appends a string at the end of a text file (UTF8) and returns
+        the number of characters added'''
     with open(filename, 'a', encoding='utf-8') as f:
         return f.write(text)
