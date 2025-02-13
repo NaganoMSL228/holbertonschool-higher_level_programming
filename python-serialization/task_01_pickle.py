436d951c800854898c695837f64f15ca2927fab4
NaganoMSL228 <9794@holbertonstudents.com>
2025-02-13 20:42:15 +0100

    test for the new script for the task 2

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,25 +1,44 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
 import pickle
 
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
 
-class Serialization:
-    '''class for serialization'''
-    @staticmethod
-    def serialize_and_save_to_file(data, filename):
-        '''function to serialize and save data to a file'''
-        with open(filename, 'wb') as file:
-            pickle.dump(data, file)
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
 
-    @staticmethod
-    def load_and_deserialize(filename):
-        '''function to load and deserialize data from a file'''
-        with open(filename, 'rb') as file:
-            return pickle.load(file)
+    def serialize(self, filename):
+        try:
+            with open(filename, 'wb') as file:
+                pickle.dump(self, file)
+        except Exception as e:
+            print(f"Serialization error: {e}")
 
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, 'rb') as file:
+                return pickle.load(file)
+        except Exception as e:
+            print(f"Deserialization error: {e}")
+            return None
 
-if __name__ == '__main__':
-    data = {'name': 'Holberton', 'type': 'school', 'topics': ('Python', 'C', 'Javascript')}
-    filename = 'data.pkl'
-    Serialization.serialize_and_save_to_file(data, filename)
-    print(Serialization.load_and_deserialize(filename))
+if __name__ == "__main__":
+    # Sample Test
+    obj = CustomObject(name="John", age=25, is_student=True)
+    print("Original Object:")
+    obj.display()
+
+    # Serialize the object
+    obj.serialize("object.pkl")
+
+    # Deserialize the object into a new instance
+    new_obj = CustomObject.deserialize("object.pkl")
+    if new_obj:
+        print("\nDeserialized Object:")
+        new_obj.display()
