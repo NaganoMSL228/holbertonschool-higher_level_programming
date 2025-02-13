f77e568b0c9f897c88248a2ff1ad13a602a6d905
NaganoMSL228 <9794@holbertonstudents.com>
2025-02-13 20:45:05 +0100

    teste for task 3

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -2,32 +2,15 @@
 import csv
 import json
 
-class CustomObject:
-    def __init__(self, name, age, is_student):
-        self.name = name
-        self.age = age
-        self.is_student = is_student
-
-    def display(self):
-        print(f"Name: {self.name}")
-        print(f"Age: {self.age}")
-        print(f"Is Student: {self.is_student}")
-
-    def serialize(self, filename):
-        try:
-            with open(filename, 'w') as file:
-                writer = csv.writer(file)
-                writer.writerow([self.name, self.age, self.is_student])
-        except Exception as e:
-            print(f"Serialization error: {e}")
-
-    @classmethod
-    def deserialize(cls, filename):
-        try:
-            with open(filename, 'r') as file:
-                reader = csv.reader(file)
-                data = list(reader)
-                return cls(data[0][0], int(data[0][1]), json.loads(data[0][2].lower()))
-        except Exception as e:
-            print(f"Deserialization error: {e}")
-            return None
+def convert_csv_to_json(csv_filename):
+    try:
+        with open(csv_filename, mode='r', newline='') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            data = [row for row in csv_reader]
+        
+        with open('data.json', mode='w') as json_file:
+            json.dump(data, json_file, indent=4)
+        
+        return True
+    except FileNotFoundError:
+        return False
