ed212b09c6ca2f6a5a686ae2ee300cb6bf5b226f
NaganoMSL228 <9794@holbertonstudents.com>
2025-03-07 16:06:41 +0100

    executable files

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,32 +1,46 @@
-
 #!/usr/bin/python3
-"""Script that adds the State object 'Louisiana' to database"""
-
+"""
+Script that adds the State object "Louisiana" to the database hbtn_0e_6_usa
+"""
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            sys.argv[1],
-            sys.argv[2],
-            sys.argv[3]
-        )
-    )
 
+if __name__ == "__main__":
+    # Check if all arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Create engine to connect to MySQL server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database),
+                           pool_pre_ping=True)
+
+    # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
+
+    # Create a Session instance
     session = Session()
 
-    # Create new State object
+    # Create a new State object
     new_state = State(name="Louisiana")
 
-    # Add and commit the new state
+    # Add the new State to the session
     session.add(new_state)
+
+    # Commit the session to save changes to the database
     session.commit()
 
-    # Print new state id
+    # Print the new state's id
     print(new_state.id)
 
+    # Close session
     session.close()
